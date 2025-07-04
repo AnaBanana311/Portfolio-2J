d10c7d05f42a1c20e2a0c4826006541038d30533
AnaBanana311 <ana.hila@escola.pr.gov.br>
2025-07-04 10:32:07 -0300

    Update and rename style.css to perguntas.js

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Musica',
+    'O que é uma colcheia?',
+    'A colcheia é uma figura ritmica'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Italiano',
+    'Como se diz irmão em italiano?',
+    'Irmão em italiano é Fratello'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
